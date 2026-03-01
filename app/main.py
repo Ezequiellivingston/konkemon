8d5d12059a4b98e03e3c2f7cd2e0e817040768a7
plivingston <plivingston@sofredigital.com.ar>
2026-03-01 11:08:00 -0300

    fix: cors

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine
 from app.models import Base
 from app.routers.users import router as users_router
@@ -6,6 +7,14 @@ from app.routers.pokemons import router as pokemons_router
 
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine)  # crea tablas
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],            
+    allow_credentials=True,
+    allow_methods=["*"],              
+    allow_headers=["*"],              
+)
+
+Base.metadata.create_all(bind=engine)  
 
 app.include_router(pokemons_router)
